987f149d38e5098b12720430a85bb7fe4a90aff7
Sudipta Basak <sudiptab2100@gmail.com>
2024-02-11 14:41:12 +0530

    Converted modExp to a view function

--- src/Counter.sol
+++ src/Counter.sol
@@ -12,30 +12,22 @@ contract Counter {
         number++;
     }
 
-    function modExp(uint256 _b, uint256 _e, uint256 _m) public returns (uint256 result) {
+    function modExp(uint256 _b, uint256 _e, uint256 _m) public view returns (uint256 _o) {
         assembly {
-            // Free memory pointer
-            let pointer := mload(0x40)
-
-            // Define length of base, exponent and modulus. 0x20 == 32 bytes
-            mstore(pointer, 0x20)
-            mstore(add(pointer, 0x20), 0x20)
-            mstore(add(pointer, 0x40), 0x20)
-
-            // Define variables base, exponent and modulus
-            mstore(add(pointer, 0x60), _b)
-            mstore(add(pointer, 0x80), _e)
-            mstore(add(pointer, 0xa0), _m)
-
-            // Store the result
-            let value := mload(0xc0)
-
-            // Call the precompiled contract 0x05 = bigModExp
-            if iszero(call(not(0), 0x05, 0, pointer, 0xc0, value, 0x20)) {
+            // define pointer
+            let p := mload(0x40)
+            // store data assembly-favouring ways
+            mstore(p, 0x20)             // Length of Base
+            mstore(add(p, 0x20), 0x20)  // Length of Exponent
+            mstore(add(p, 0x40), 0x20)  // Length of Modulus
+            mstore(add(p, 0x60), _b)  // Base
+            mstore(add(p, 0x80), _e)     // Exponent
+            mstore(add(p, 0xa0), _m)     // Modulus
+            if iszero(staticcall(not(0), 0x05, p, 0xc0, p, 0x20)) {
                 revert(0, 0)
             }
-
-            result := mload(value)
+            // data
+            _o := mload(p)
         }
     }
 
